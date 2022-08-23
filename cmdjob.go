b6c059d2a375e1062a1b337a4233f49be8fdc8da
J <18084450+JelmerDeHen@users.noreply.github.com>
2022-08-23 15:43:35 +0200

    gofmt

--- cmdjob.go
+++ cmdjob.go
@@ -1,128 +1,125 @@
 package xidle
 
 import (
-  "bytes"
+	"bytes"
 	"fmt"
-  "log"
+	"log"
 	"os/exec"
 	"time"
 )
 
-
 // Helper for managing apps with Idlemon
 type CmdJob struct {
-  name string
-  arg []string
+	name string
+	arg  []string
 
-  cmd *exec.Cmd
-  stdcombined bytes.Buffer
+	cmd         *exec.Cmd
+	stdcombined bytes.Buffer
 
-  // state
-  start time.Time
-  etime time.Duration
+	// state
+	start time.Time
+	etime time.Duration
 
-  etimeMax time.Duration
+	etimeMax time.Duration
 
-  Dbg bool
+	Dbg bool
 }
 
 func (c *CmdJob) Spawn() {
-  if c.Running() {
-    return
-  }
+	if c.Running() {
+		return
+	}
 
-  //if c.Dbg {
-    log.Printf("Spawn(): %s %v\n", c.name, c.arg)
-  //}
+	//if c.Dbg {
+	log.Printf("Spawn(): %s %v\n", c.name, c.arg)
+	//}
 
-  // Kill before exec
-  if c.Running() {
-    c.Kill()
-  }
+	// Kill before exec
+	if c.Running() {
+		c.Kill()
+	}
 
-  c.cmd = exec.Command(c.name, c.arg...)
+	c.cmd = exec.Command(c.name, c.arg...)
 
-  c.cmd.Stdout = &c.stdcombined
-  c.cmd.Stderr = &c.stdcombined
+	c.cmd.Stdout = &c.stdcombined
+	c.cmd.Stderr = &c.stdcombined
 
 	go c.cmd.Run()
 
-  // Give time to fail
+	// Give time to fail
 	time.Sleep(time.Second * 1)
 
-  if !c.Running() {
-	  err := fmt.Errorf("Could not start %s: errno=%d\n%s", c.name, c.cmd.ProcessState.ExitCode(), c.stdcombined.String())
+	if !c.Running() {
+		err := fmt.Errorf("Could not start %s: errno=%d\n%s", c.name, c.cmd.ProcessState.ExitCode(), c.stdcombined.String())
 		panic(err)
-  }
+	}
 
 	c.start = time.Now()
 }
 
 func (c *CmdJob) Poll() {
-  // Elapsed time
-  c.etime = time.Since(c.start)
+	// Elapsed time
+	c.etime = time.Since(c.start)
 
-  // Kill process to rotate outfile
-  if c.etime > c.etimeMax {
-    if c.Dbg {
-      log.Printf("Process elapsed time limit reached: c.etime=%vs > c.etimeMax%vs\n", c.etime.Seconds(), c.etimeMax.Seconds())
-    }
-    c.Kill()
-  }
+	// Kill process to rotate outfile
+	if c.etime > c.etimeMax {
+		if c.Dbg {
+			log.Printf("Process elapsed time limit reached: c.etime=%vs > c.etimeMax%vs\n", c.etime.Seconds(), c.etimeMax.Seconds())
+		}
+		c.Kill()
+	}
 
-  //fmt.Printf("%+v\n", c)
+	//fmt.Printf("%+v\n", c)
 }
 
-
 func (c *CmdJob) Kill() {
-  if !c.Running() {
-    return
-  }
-  //if c.Dbg {
-    log.Printf("Kill(): %s %v\n", c.name, c.arg)
-  //}
-  c.cmd.Process.Kill()
+	if !c.Running() {
+		return
+	}
+	//if c.Dbg {
+	log.Printf("Kill(): %s %v\n", c.name, c.arg)
+	//}
+	c.cmd.Process.Kill()
 }
 
 func (c *CmdJob) Running() bool {
-  if c.cmd == nil {
-    return false
-  }
+	if c.cmd == nil {
+		return false
+	}
 
-  if c.cmd != nil && c.cmd.ProcessState != nil {
-  //if c.cmd != nil && c.cmd.ProcessState != nil && c.cmd.ProcessState.Exited() {
-    return false
-  }
+	if c.cmd != nil && c.cmd.ProcessState != nil {
+		//if c.cmd != nil && c.cmd.ProcessState != nil && c.cmd.ProcessState.Exited() {
+		return false
+	}
 
-  return true
+	return true
 }
 
 func NewCmdJob(name string, arg ...string) *CmdJob {
-  return &CmdJob{
-    name: name,
-    arg: arg,
-    start: time.Now(),
-    etimeMax: time.Hour,
-    // testing
-    //etimeMax: time.Second * 20,
-  }
+	return &CmdJob{
+		name:     name,
+		arg:      arg,
+		start:    time.Now(),
+		etimeMax: time.Hour,
+		// testing
+		//etimeMax: time.Second * 20,
+	}
 }
 
 func NewIdlemon(runner *CmdJob) *Idlemon {
-  idlecmd := &Idlemon{
-    PollT: time.Second,
-    IdleLessT: time.Minute,
-    IdleOverT: time.Minute*10,
+	idlecmd := &Idlemon{
+		PollT:     time.Second,
+		IdleLessT: time.Minute,
+		IdleOverT: time.Minute * 10,
 
-    Poll: runner.Poll,
-    IdleLess: runner.Spawn,
-    IdleOver: runner.Kill,
-  }
+		Poll:     runner.Poll,
+		IdleLess: runner.Spawn,
+		IdleOver: runner.Kill,
+	}
 
-  // testing
-  //idlecmd.IdleLessT = time.Second * 1
-  //idlecmd.IdleOverT = time.Second * 3
+	// testing
+	//idlecmd.IdleLessT = time.Second * 1
+	//idlecmd.IdleOverT = time.Second * 3
 
-  return idlecmd
+	return idlecmd
 }
-
--- idlemon.go
+++ idlemon.go
@@ -1,10 +1,10 @@
 package xidle
 
 import (
-  "log"
+	"log"
 	"time"
 
-  "github.com/JelmerDeHen/scrnsaver"
+	"github.com/JelmerDeHen/scrnsaver"
 )
 
 // Define callbacks:
@@ -12,22 +12,22 @@ import (
 // IdleLess() is executed when idle time < IdleLessT
 // IdleOver() is executed when idle time > IdleOverT
 type Idlemon struct {
-  Poll func()
-  PollT time.Duration
-  IdleLess func()
-  IdleLessT time.Duration
-  IdleOver func()
-  IdleOverT time.Duration
+	Poll      func()
+	PollT     time.Duration
+	IdleLess  func()
+	IdleLessT time.Duration
+	IdleOver  func()
+	IdleOverT time.Duration
 
-  Dbg bool
+	Dbg bool
 }
 
 // Run defined callbacks based on how long user is idle
-func (im *Idlemon) Run () {
-  if im.Poll == nil || im.IdleLess == nil || im.IdleOver == nil {
-    log.Println("Define callbacks")
-    return
-  }
+func (im *Idlemon) Run() {
+	if im.Poll == nil || im.IdleLess == nil || im.IdleOver == nil {
+		log.Println("Define callbacks")
+		return
+	}
 
 	for {
 		// Get idle time
@@ -36,23 +36,23 @@ func (im *Idlemon) Run () {
 			panic(err)
 		}
 
-    im.Poll()
+		im.Poll()
 
-    // When idle less than duration
+		// When idle less than duration
 		if info.Idle < im.IdleLessT {
-      if im.Dbg {
-        log.Printf("User present: info.Idle=%vs < %v\n", info.Idle.Seconds(), im.IdleLessT.Seconds())
-      }
-      im.IdleLess()
+			if im.Dbg {
+				log.Printf("User present: info.Idle=%vs < %v\n", info.Idle.Seconds(), im.IdleLessT.Seconds())
+			}
+			im.IdleLess()
 		}
 
-    // When idle over duration
-    if info.Idle > im.IdleOverT {
-      if im.Dbg {
-        log.Printf("User idle: info.Idle=%vs > %v\n", info.Idle.Seconds(), im.IdleOverT.Seconds())
-      }
-      im.IdleOver()
-    }
+		// When idle over duration
+		if info.Idle > im.IdleOverT {
+			if im.Dbg {
+				log.Printf("User idle: info.Idle=%vs > %v\n", info.Idle.Seconds(), im.IdleOverT.Seconds())
+			}
+			im.IdleOver()
+		}
 
 		time.Sleep(im.PollT)
 	}
